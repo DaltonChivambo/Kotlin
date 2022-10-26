6d39f91b2595ce4fd0ff8c486e78328c88e0f8f4
dalton <daltonchivambo@gmail.com>
2022-10-26 05:33:52 +0200

    When

---
+++ src/main/kotlin/When.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+
+
+    var option = 1
+
+    when(option){
+        1 -> println("Orange Juice")
+        2 -> println("Grape Juice")
+        3 -> println("StrawBerry Juice")
+        else -> println("Invalid option")
+    }
+}
