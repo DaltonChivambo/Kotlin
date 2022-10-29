9beb80b69470c33e3d9b6a694ee07ca8b3e8e4aa
dalton <daltonchivambo@gmail.com>
2022-10-29 21:57:37 +0200

    making class in java and kotlin

--- src/main/kotlin/Class/Person.kt
+++ src/main/kotlin/Class/Person.kt
@@ -5,10 +5,10 @@ class Person (
         )
 
 fun main(){
-    val person: Person = Person("DALTON",20)
-    val person2: Person = Person("Viriato",54)
+    val person = Person("DALTON",20)
+    val person2 = Person("Viriato",54)
 
-    println("Person 1: ${person.name}")
-    println("Person 2: ${person2.name}")
+    println("Person 1: ${person.name} have ${person.age} years old")
+    println("Person 2: ${person2.name} have ${person.age} years old")
 
 }
